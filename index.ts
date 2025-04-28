7133c22a76369901e544bd61f18547999905a34d
ManuelPerezF <A01741957@tec.mx>
2025-04-28 08:26:58 -0600

    handler terminado

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
-//import calculoRoutes from './src/routes/calculoRoutes';
+import authRoutes from './src/routes/authRoutes';
 
 const app = express();
 const PORT = process.env.PORT || 7000;
 
 app.use(express.json());
-//app.use ('/api', calculoRoutes);
+app.use('/api', authRoutes);
 
 app.listen(PORT, () => console.log('Server running'))
--- src/handler/authHandler.ts
+++ src/handler/authHandler.ts
@@ -9,8 +9,25 @@ export class AuthHandler {
     }
 
     async login(req: Request, res: Response, next: NextFunction) {
-        
+        try {
+            const { username, password } = req.body;
+
+            const employee = await this.authController.login(username, password);
+
+            if (employee) {
+                res.status(200).json({
+                    message: 'Inicio de sesión exitoso.',
+                    employee: {
+                        name: employee.name,
+                        email: employee.email,
+                    },
+                });
+            } 
+            else {
+                res.status(401).json({ error: 'Nombre de usuario o contraseña incorrectos.' });
+            }
+        } catch (error) {
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
     }
-
-
 }
